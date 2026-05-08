c5eda87f51a75b29059528db05524507aed60f9d
Jan Wech <jan.wech@gmx.de>
2026-05-08 21:53:11 +0200

    Implemented ActivationFuntion Class as abstract Class

--- NN/Activation/activation.py
+++ NN/Activation/activation.py
@@ -1,18 +1,31 @@
 import numpy
+from abc import ABC, abstractmethod
+
+class ActivationFunction(ABC):
+
+    @staticmethod
+    @abstractmethod
+    def forward(z_input: numpy.ndarray):
+        pass
+
+    @staticmethod
+    @abstractmethod
+    def backward(a_output: numpy.ndarray):
+        pass
+
+class Sigmoid(ActivationFunction):
+
+    @staticmethod
+    def forward(z_input: numpy.ndarray):
+
+        a = 1 / (1 + numpy.exp(-z_input))
+        return a
+
+    @staticmethod
+    def backward(a_output: numpy.ndarray):
+
+        dz = 1 / (1 + numpy.exp(-a_output)) * (1 - 1 / (1 + numpy.exp(-a_output)))
+
+        return dz
 
 
-def select_activation(activation: str):
-
-    match activation:
-
-        case "sigmoid":
-            return sigmoid
-
-        case _:
-            return "No activation function with this name"
-
-def sigmoid(z_input: numpy.ndarray):
-
-    a = 1 / (1 + numpy.exp(-z_input))
-
-    return a
