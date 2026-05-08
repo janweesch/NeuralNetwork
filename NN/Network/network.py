caaf2d375ef6b63b50063d198f09fc0b897aae5f
Jan Wech <jan.wech@gmx.de>
2026-05-08 21:49:28 +0200

    Implemented Backward Pass...

--- NN/Network/network.py
+++ NN/Network/network.py
@@ -1,12 +1,12 @@
 import numpy
 from typing import List
 from NN.Layer.layer import Layer
-from NN.Activation.activation import select_activation
+from NN.Activation.activation import ActivationFunction
 
 
 class Network:
 
-    def __init__(self, x_input: int, classes: int,  hidden_layers: int, hidden_sizes: List[int], activations: List[str], output_activation: str ):
+    def __init__(self, x_input: int, classes: int,  hidden_layers: int, hidden_sizes: List[int], activations: List[ActivationFunction], output_activation: ActivationFunction ):
 
         assert hidden_layers == len(hidden_sizes) and hidden_layers == len(activations), "Length of hidden_layers list must be equal to length of hidden_size list!"
 
@@ -15,7 +15,6 @@ class Network:
 
         """Initialize the Neural Network, with all hidden layers and corresponding activation functions."""
         for index, hidden_size, activation in enumerate(zip(hidden_sizes, activations)):
-            activation = select_activation(activation) # activation function
 
             current_layer = Layer(input_dim=prev_size, output_dim=hidden_size, activation_function=activation) # initialize layer
             self.layers.append(current_layer)
@@ -23,7 +22,6 @@ class Network:
             prev_size = hidden_size # overwrite previous size
 
         self.layers.append(Layer(input_dim=prev_size, output_dim=classes, activation_function=output_activation))  # output Layer
-
 
     def forward(self, x_input: numpy.ndarray):
         """Forward pass
@@ -36,6 +34,20 @@ class Network:
         return output
 
     def backward(self, loss: float):
+        """
+        Backward pass
+        """
+        dout = loss
+        for layer in self.layers[::-1]:
+            dout = layer.backward(dout=dout)
+
+        return dout
+
+
+
+
+
+
 
 
 
