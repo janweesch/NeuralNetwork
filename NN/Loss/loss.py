71e876bb4ef3e525123c7355935ece47dc0a655f
Jan Wech <jan.wech@gmx.de>
2026-05-21 18:45:36 +0200

    Function staticmethod

--- NN/Loss/loss.py
+++ NN/Loss/loss.py
@@ -3,22 +3,26 @@ import numpy
 
 class Loss:
 
-    def forward(self, y_out, y_truth):
+    @staticmethod
+    def forward(y_out, y_truth):
         return NotImplementedError
 
-    def backward(self, y_out, y_truth):
+    @staticmethod
+    def backward(y_out, y_truth):
         return NotImplementedError
 
 class L1(Loss):
 
-    def forward(self, y_out, y_truth):
+    @staticmethod
+    def forward(y_out, y_truth):
 
         loss = - numpy.abs(y_out - y_truth)
         average_loss = numpy.mean(loss)
 
         return average_loss
 
-    def backward(self, y_out, y_truth):
+    @staticmethod
+    def backward(y_out, y_truth):
 
         dl = None
 
@@ -35,14 +39,16 @@ class L1(Loss):
 
 class BCE(Loss):
 
-    def forward(self, y_out, y_truth):
+    @staticmethod
+    def forward(y_out, y_truth):
 
         loss = - (y_truth * numpy.log(y_out) + (1-y_truth) * (1-numpy.log(y_out))) # binary cross entropy
         average_loss = numpy.mean(loss) # accumulated loss
 
         return average_loss
 
-    def backward(self, y_out, y_truth):
+    @staticmethod
+    def backward(y_out, y_truth):
 
         # Don`t sum up the gradient, one gradient for each sample.
         # 1/N in front of the loss is necessary to average the loss without changing the learning Rate for every Batch
