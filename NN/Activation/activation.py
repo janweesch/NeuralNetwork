cdec26b64cdfbcea1f2aac443d5fbb27038f20e5
Jan Wech <jan.wech@gmx.de>
2026-06-17 15:21:04 +0200

    updated activation layer...

--- NN/Activation/activation.py
+++ NN/Activation/activation.py
@@ -1,69 +1,87 @@
-import numpy
+import numpy as np
 from abc import ABC, abstractmethod
 
 class ActivationFunction(ABC):
 
     @staticmethod
     @abstractmethod
-    def forward(z_input: numpy.ndarray):
+    def forward(z_input: np.ndarray):
         pass
 
     @staticmethod
     @abstractmethod
-    def backward(z_input: numpy.ndarray):
+    def backward(z_input: np.ndarray):
         pass
 
 class Affine(ActivationFunction):
 
     @staticmethod
-    def forward(z_input: numpy.ndarray):
+    def forward(z_input: np.ndarray):
         return z_input
 
     @staticmethod
-    def backward(z_input: numpy.ndarray):
-        return numpy.ones((z_input.shape[0], 1))
+    def backward(z_input: np.ndarray):
+        return np.ones((z_input.shape[0], 1))
 
 class Sigmoid(ActivationFunction):
 
     @staticmethod
-    def forward(z_input: numpy.ndarray):
+    def forward(z_input: np.ndarray):
 
-        a = 1 / (1 + numpy.exp(-z_input))
+        a = 1 / (1 + np.exp(-z_input))
         return a
 
     @staticmethod
-    def backward(z_input: numpy.ndarray):
+    def backward(z_input: np.ndarray):
 
-        dz = 1 / (1 + numpy.exp(-z_input)) * (1 - (1 / (1 + numpy.exp(-z_input))))
+        dz = 1 / (1 + np.exp(-z_input)) * (1 - (1 / (1 + np.exp(-z_input))))
 
         return dz
 
 class ReLU(ActivationFunction):
 
     @staticmethod
-    def forward(z_input: numpy.ndarray):
+    def forward(z_input: np.ndarray):
 
-        a = numpy.maximum (0, z_input)
+        a = np.maximum (0, z_input)
 
         return a
 
     @staticmethod
-    def backward(z_input: numpy.ndarray):
+    def backward(z_input: np.ndarray):
 
-        dz = numpy. where(z_input > 0, 1, 0)
+        dz = np.where(z_input > 0, 1, 0)
+
+        return dz
+    
+class Tanh(ActivationFunction):
+    
+    @staticmethod
+    def forward(z_input: np.ndarray):
+        
+        a = (np.exp(z_input)-np.exp(-z_input)) / (np.exp(z_input)+np.exp(-z_input))
+
+        return a
+
+    @staticmethod
+    def backward(z_input):
+
+        dz = 1 - np.pow((np.exp(z_input)-np.exp(-z_input)) / (np.exp(z_input)+np.exp(-z_input)), 2)
 
         return dz
 
 class Quadratic:
 
     @staticmethod
-    def forward(z_input: numpy.ndarray):
+    def forward(z_input: np.ndarray):
+
         a = z_input**2
 
         return a
 
     @staticmethod
-    def backward(z_input:numpy.ndarray):
+    def backward(z_input:np.ndarray):
+
         dz = 2*z_input
 
         return dz
